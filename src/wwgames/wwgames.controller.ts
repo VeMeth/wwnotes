e61eaa142af0c13fb9b0093f6f48293d8b52b8ed
VeMeth <noerd666@gmail.com>
2021-09-04 14:57:21 +0200

    Files forgotten

--- src/wwgames/wwgames.controller.ts
+++ src/wwgames/wwgames.controller.ts
@@ -8,11 +8,11 @@ import { WwgamesService } from './wwgames.service';
 @Controller('wwgames')
 export class WwgamesController {
   constructor(private WwgamesService: WwgamesService) {}
-  @Post()
+  @Get('game')
   @ApiOkResponse({ description: 'Game created' })
   @ApiNotFoundResponse({ description: 'Event with provided ID not found' })
   @ApiBadRequestResponse({ description: 'Invalid Game ID' })
-  public async GetGame(GameId) {
+  public async createGame(GameId) {
     return this.WwgamesService.createGame(GameId);
   }
 }
--- src/wwgames/wwgames.service.ts
+++ src/wwgames/wwgames.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
-import { AxiosResponse } from 'axios';
-import { map } from 'rxjs';
 import { Model } from 'mongoose';
 import { Iwwngames } from './interfaces/wwgames.interfaces';
 import { InjectModel } from '@nestjs/mongoose';
+import { map } from 'rxjs';
+import { response } from 'express';
 
 @Injectable()
 export class WwgamesService {
@@ -14,20 +14,26 @@ export class WwgamesService {
     private readonly wwgamesModel: Model<Iwwngames>,
   ) {}
 
-  public async createGame(GameId: string): Promise<any> {
-    console.log('Post request');
-    return await this.httpService
+  public async createGame(GameId: string) {
+    console.log('Create Game');
+    const plist = await this.httpService
       .get(
         'https://archive.werewolv.es/extra/liveview/api/game/parsed/smc-400.json',
       )
-      .pipe(map((response) => response.data));
-
-    /*const result = await this.httpService
-      .post('http://localhost:3000/wwnotes', {
-        name: 'Boop',
-      })
-      .pipe(map((response) => response.data));
-    console.log(result); */
-    //return plist;
+      .pipe(map((response) => response.data.game.identities));
+    plist.subscribe({
+      next: (identities) => {
+        identities.forEach((element) => {
+          console.log('Adding Note for ', element);
+          this.httpService
+            .post('http://localhost:3000/wwnotes', {
+              name: element,
+              role: 'Unknown',
+            })
+            .pipe(map((response) => response.data));
+        });
+      },
+    });
+    return response;
   }
 }
