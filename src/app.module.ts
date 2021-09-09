3f70389d5508680cd87086f2e6418500eff02e8c
VeMeth <noerd666@gmail.com>
2021-09-10 00:15:16 +0200

    Changed DB parameters to dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,27 @@ import { WwnotesModule } from './wwnotes/wwnotes.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { WwgamesModule } from './wwgames/wwgames.module';
 import { HttpService } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
+
+console.log(process.env.MONGO_HOST);
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     WweventsModule,
     WwnotesModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/wwnotes'),
+    MongooseModule.forRoot(
+      'mongodb://' +
+        process.env.MONGO_USER +
+        ':' +
+        process.env.MONGO_PASSWORD +
+        '@' +
+        process.env.MONGO_HOST +
+        ':' +
+        process.env.MONGO_PORT +
+        '/wwnotes',
+    ),
     WwgamesModule,
   ],
 })
 export class AppModule {}
+console.log(process.env.MONGO_HOST);
