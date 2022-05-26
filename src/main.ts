e8b611798dac80c8467cfa90b533174361331d54
VeMeth <noerd666@gmail.com>
2022-05-26 21:08:46 +0200

    Updated Dependencies

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 declare const module: any;
-
+console.log('Cors host: ' + process.env.CORS_HOST);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
